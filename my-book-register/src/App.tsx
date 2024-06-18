d2ed37a5aaec067d51016356b387541dd5928c45
ya-fukuda <ya-fukuda@kentem.co.jp>
2024-06-19 08:29:21 +0900

    不要なimport削除

--- my-book-register/src/App.tsx
+++ my-book-register/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css';
 import BookRegister from './components/bookRegister';
 import FilterableBookTable from './components/filterableBookTable';
